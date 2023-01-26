c0d96818804186023c85ba3025566ca229849448
kwestsnowflake <123594797+kwestsnowflake@users.noreply.github.com>
2023-01-26 13:59:19 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,14 +48,22 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
   
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
+    return 'Thanks for adding '+ new_fruit
+  
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  streamlit.text(insert_row_snowflake(add_my_fruit))
 # 
 # my_cur.execute("SELECT * FROM fruit_load_list")
 # my_data_rows = my_cur.fetchall()
 # streamlit.text("The fruit load list contains:")
 # streamlit.text(my_data_rows)
 
-# add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 # streamlit.write('The user entered ', add_my_fruit)
 
-#my_cur.execute("insert into fruit_load_list values ('test')")
+#
 
